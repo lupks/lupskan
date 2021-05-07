e8e6b54234c61ba7ae1caa6cd2efcb62acfbef23
lupks <jeffreylupker@gmail.com>
2021-05-07 14:55:58 -0400

    fixed sockets

--- wsgi.py
+++ wsgi.py
@@ -4,7 +4,3 @@ import os
 if __name__ == "__main__":
     socketio.run(app, debug=True)
 
-# if __name__ == "__main__":
-#     socketio.run(app, debug=True, port=int(os.environ.get('PORT', '5000')))
-
-
